647bdcd23d877ffde985d130716c1e2aa2e2ca37
Renaat De Vos <renaat@drvn.be>
2024-07-25 14:05:10 +0200

    updated typo in generated JS code

--- app.py
+++ app.py
@@ -174,10 +174,10 @@ def generate_faq_code(faqs):
                 <svg class='arrow' viewBox='0 0 320 512' width='16' title='angle-down'>
                     <path d='M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z'/>
                 </svg>
-                <span>{faq['Vraag']}</span>
+                <span>{faq['question']}</span>
             </button>
             <div class='accordeon-body'>
-                <p>{faq['Amtwoord']}</p>
+                <p>{faq['answer']}</p>
             </div>
         </div>
         """
